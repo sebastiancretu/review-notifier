3212ad073554ac4f6fff0ac239ed235708d46cf5
Sebastian Cretu <cretu.sebastian@gmail.com>
2023-04-24 08:19:18 +0200

    fix: markdown

--- src/slack-message.ts
+++ src/slack-message.ts
@@ -87,7 +87,7 @@ export class SlackMessage {
           elements: [
             {
               type: 'mrkdwn',
-              text: `> ${body}`,
+              text: body,
             },
           ],
         },
